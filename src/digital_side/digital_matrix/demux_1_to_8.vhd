76d5b0b788adbe23f8bb55485d78713c99947e0c
Robert D Jordan <rob.d.jordan@gmail.com>
2023-02-09 10:38:09 +1100

    Update demux_1_to_8.vhd

--- src/digital_side/digital_matrix/demux_1_to_8.vhd
+++ src/digital_side/digital_matrix/demux_1_to_8.vhd
@@ -1,28 +1,28 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity demux_1_to_8 is
+entity mux_8_to_1 is
     Port ( 
-        data: in  std_logic;
+        data: in  std_logic_vector(7 downto 0);
         sel: in  std_logic_vector(2 downto 0);
-        demux_out: out  std_logic_vector(7 downto 0)
+        mux_out: out  std_logic
     );
-end demux_1_to_8;
+end mux_8_to_1;
 
-architecture Behavioral of demux_1_to_8 is
+architecture Behavioral of mux_8_to_1 is
 begin
     process(sel, data)
     begin
-        demux_out <= (others => '0');
         case sel is
-            when "000" => demux_out(0) <= data;
-            when "001" => demux_out(1) <= data;
-            when "010" => demux_out(2) <= data;
-            when "011" => demux_out(3) <= data;
-            when "100" => demux_out(4) <= data;
-            when "101" => demux_out(5) <= data;
-            when "110" => demux_out(6) <= data;
-            when "111" => demux_out(7) <= data;
+            when "000" => mux_out <= data(0);
+            when "001" => mux_out <= data(1);
+            when "010" => mux_out <= data(2);
+            when "011" => mux_out <= data(3);
+            when "100" => mux_out <= data(4);
+            when "101" => mux_out <= data(5);
+            when "110" => mux_out <= data(6);
+            when "111" => mux_out <= data(7);
+            when others => mux_out <= '0';
         end case;
     end process;
 end Behavioral;
