ed0e98a6d74838243496cf9702432040cc62536d
Robert D Jordan <rob.d.jordan@gmail.com>
2023-05-29 16:48:38 +1000

    Update ycrcr2rgb_simple.vhd

--- src/video_output/ycrcr2rgb_simple.vhd
+++ src/video_output/ycrcr2rgb_simple.vhd
@@ -44,13 +44,13 @@ end ycrcr2rgb_simple;
 
 architecture Behavioral of ycrcr2rgb_simple is
 
-signal y_i  : integer range 0 to 254;
-signal cr_i : integer range 0 to 254;
-signal cb_i : integer range 0 to 254;
+signal y_i  : integer range 0 to 255;
+signal cr_i : integer range 0 to 255;
+signal cb_i : integer range 0 to 255;
 
-signal r_i : integer range 0 to 254;
-signal g_i : integer range 0 to 254;
-signal b_i : integer range 0 to 254;
+signal r_i : integer range 0 to 255;
+signal g_i : integer range 0 to 255;
+signal b_i : integer range 0 to 255;
 
 begin
 y_i <= to_integer(unsigned(y));
@@ -65,24 +65,24 @@ process (r_i, g_i, b_i) is
 begin
     if r_i < 0 then
         r <= std_logic_vector(to_unsigned(0, r'length)); 
-    elsif  r_i > 254 then
-        r <= std_logic_vector(to_unsigned(254, r'length));
+    elsif  r_i > 255 then
+        r <= std_logic_vector(to_unsigned(255, r'length));
     else
         r <= std_logic_vector(to_unsigned(r_i, r'length));
     end if;
     
     if g_i < 0 then
         g <= std_logic_vector(to_unsigned(0, g'length)); 
-    elsif  g_i > 254 then
-        g <= std_logic_vector(to_unsigned(254, g'length));
+    elsif  g_i > 255 then
+        g <= std_logic_vector(to_unsigned(255, g'length));
     else
         g <= std_logic_vector(to_unsigned(g_i, g'length));
     end if;
     
         if b_i < 0 then
         b <= std_logic_vector(to_unsigned(0, b'length)); 
-    elsif  b_i > 254 then
-        b <= std_logic_vector(to_unsigned(254, b'length));
+    elsif  b_i > 255 then
+        b <= std_logic_vector(to_unsigned(255, b'length));
     else
         b <= std_logic_vector(to_unsigned(b_i, b'length));
     end if;
