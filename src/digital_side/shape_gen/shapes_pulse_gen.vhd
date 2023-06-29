b6bc0ac401e3008a52a1e9d6b5a3296d1efde685
robertdjordanbitbucket <robertjo@blackmagicdesign.com>
2023-06-29 15:34:30 +1000

    random ramp reset uses NCO, need to check the actual offset that looks like it happens after the first peak, but for now is good

--- src/digital_side/shape_gen/shapes_pulse_gen.vhd
+++ src/digital_side/shape_gen/shapes_pulse_gen.vhd
@@ -123,27 +123,27 @@ begin
        -- end if; 
     end process;
     
-    process (clk, rst) -- random reset ramp logic, counts up till zoom value then uses mux to jump up and rand oddly, needs beter method
-    -- need to jus tuse a ramp get osc reset by the pulse active at 0 and the freq controlled by zoom
+--    process (clk, rst) -- random reset ramp logic, counts up till zoom value then uses mux to jump up and rand oddly, needs beter method
+--    -- need to jus tuse a ramp get osc reset by the pulse active at 0 and the freq controlled by zoom
     
-    begin
-        if pulse_active = '0' then
-          ramp_rst <= '1';
-        elsif rising_edge(clk) then       
-            if(pulse_counter mod unsigned(zoom) = 0 ) then
-                ramp_rst <= '1';
+--    begin
+--        if pulse_active = '0' then
+--          ramp_rst <= '1';
+--        elsif rising_edge(clk) then       
+--            if(pulse_counter mod unsigned(zoom) = 0 ) then
+--                ramp_rst <= '1';
                 
             
-            elsif (pulse_counter > unsigned(zoom) ) then
-                ramp_rst <= '0';
-                rst_ramp_mux <= rst_ramp_out(4 downto 0) & "0000" ;            
-                else
-                ramp_rst <= '0';
-                rst_ramp_mux <= rst_ramp_out(4 downto 0) & "0000" ;
-            end if;
-        end if;
+--            elsif (pulse_counter > unsigned(zoom) ) then
+--                ramp_rst <= '0';
+--                rst_ramp_mux <= rst_ramp_out(4 downto 0) & "0000" ;            
+--                else
+--                ramp_rst <= '0';
+--                rst_ramp_mux <= rst_ramp_out(4 downto 0) & "0000" ;
+--            end if;
+--        end if;
         
-    end process;
+--    end process;
     
     -- NOISE GENERATOR
     noise_gen : entity work.rand_num
@@ -156,15 +156,13 @@ begin
             q => noise_out
         );
     -- Random reset ramp * is it actual random?
-        rst_ramp: entity work.counter
-        generic map (
-            width => 9
-        )
+        rst_ramp: entity work.nco
         port map (
-            clk => clk,
-            rst => ramp_rst,
-            enable => '1',
-            count => rst_ramp_out
+            i_clk => clk,
+            i_rstb => rst,
+            i_sync_reset =>  pulse_active,
+            i_fcw => zoom,
+            o_nco => rst_ramp_out
         );
 
     pulse_len_int <= unsigned(pulse_len);
