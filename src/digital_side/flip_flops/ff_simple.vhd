fdccd04cb8999485730bc925990208f3b34a5b57
Robert D Jordan <rob.d.jordan@gmail.com>
2023-09-05 10:52:02 +1000

    Create ff_simple.vhd

---
+++ src/digital_side/flip_flops/ff_simple.vhd
@@ -0,0 +1,37 @@
+--a spimplified version of the EMS FF
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity ff_simple is
+    Port ( clk : in STD_LOGIC;
+           trig : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           ff_out : out STD_LOGIC);
+end ff_simple;
+
+architecture Behavioral of ff_simple is
+
+signal ff_state_int : std_logic := '0';
+signal trig_d : std_logic := '0';
+
+begin
+
+process(clk)
+    begin
+    if (rising_edge(clk)) then
+        if (rst = '1') then
+            ff_state_int <= '0';
+        else
+        trig_d <= trig;
+            if trig_d = '0' and trig = '1' then
+                ff_state_int <= NOT ff_state_int;
+            end if;
+        end if;
+    end if;
+end process;
+
+ff_out <= ff_state_int;
+end Behavioral;
