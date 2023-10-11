a72a266a39b95e8be00935924fcf0fc7a666365d
robertdjordanbitbucket <robertjo@blackmagicdesign.com>
2023-10-11 16:44:15 +1100

    formatting

--- src/analog_side/analog_side.vhd
+++ src/analog_side/analog_side.vhd
@@ -17,25 +17,24 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.ALL;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.numeric_std.all;
 library work;
 use work.array_pck.all;
 
 entity analog_side is
- Port ( 
- 
-    clk      :in    STD_LOGIC;
-    rst      :in    STD_LOGIC;
-    wr       :in    STD_LOGIC;
-    out_addr     :in     std_logic_vector(3 downto 0) ;
-    ch_addr      :in     std_logic_vector(3 downto 0);
-    gain_in  :in    std_logic_vector(4 downto 0);
+  port
+  (
+
+    clk          : in std_logic;
+    rst          : in std_logic;
+    wr           : in std_logic;
+    out_addr     : in std_logic_vector(3 downto 0);
+    ch_addr      : in std_logic_vector(3 downto 0);
+    gain_in      : in std_logic_vector(4 downto 0);
     mixer_inputs : in array_12(9 downto 0);
-    outputs : out array_12(9 downto 0)  -- 12-bit wide outputs
+    outputs      : out array_12(9 downto 0) -- 12-bit wide outputs
 
     --analoge controls from reg file
     pos_h_1   : in std_logic_vector(8 downto 0);
@@ -55,7 +54,7 @@ entity analog_side is
     gear_2    : in std_logic_vector(8 downto 0);
     lantern_2 : in std_logic_vector(8 downto 0);
     fizz_2    : in std_logic_vector(8 downto 0);
-  
+
     noise_freq    : in std_logic_vector(9 downto 0);
     slew_in       : in std_logic_vector(2 downto 0);
     cycle_recycle : in std_logic;
@@ -63,59 +62,62 @@ entity analog_side is
     y_level  : in std_logic_vector(11 downto 0);
     cr_level : in std_logic_vector(11 downto 0);
     cb_level : in std_logic_vector(11 downto 0);
-    
- );
+
+  );
 end analog_side;
 
 architecture Behavioral of analog_side is
-   signal out_addr_int     :   integer;
-   signal ch_addr_int      :   integer;
+  signal out_addr_int : integer;
+  signal ch_addr_int  : integer;
 
-   signal noise_1 : std_logic_vector(9 downto 0);
-   signal noise_2 : std_logic_vector(9 downto 0);
-   
+  signal noise_1 : std_logic_vector(9 downto 0);
+  signal noise_2 : std_logic_vector(9 downto 0);
+
 begin
 
-    out_addr_int <= to_integer(unsigned(out_addr));
-    ch_addr_int <= to_integer(unsigned(ch_addr));
-    
-analox_matrix : entity work.mixer_interface
-  Port map( 
-    clk => clk,
-      rst => rst,
-      wr => wr,
-      out_addr => out_addr_int,
-      ch_addr => ch_addr_int,
-      gain_in => gain_in,
+  out_addr_int <= to_integer(unsigned(out_addr));
+  ch_addr_int  <= to_integer(unsigned(ch_addr));
+
+  analox_matrix : entity work.mixer_interface
+    port map
+    (
+      clk          => clk,
+      rst          => rst,
+      wr           => wr,
+      out_addr     => out_addr_int,
+      ch_addr      => ch_addr_int,
+      gain_in      => gain_in,
       mixer_inputs => mixer_inputs,
-      outputs => outputs
-  );
+      outputs      => outputs
+    );
 
   random_1 : entity work.random_voltage
-    port map (
-      Clock      => clk     , 
-      rst        => rst       ,
-      recycle    => cycle_recycle   ,  
-      noise_freq => noise_freq,
-      slew_in    => slew_in   , 
-      noise_1    => noise_1   ,
-      noise_2    => noise_2   ,
-      );
+    port
+    map (
+    Clock      => clk,
+    rst        => rst,
+    recycle    => cycle_recycle,
+    noise_freq => noise_freq,
+    slew_in    => slew_in,
+    noise_1    => noise_1,
+    noise_2    => noise_2,
+    );
 
-      YUV_out_levels : entity work.YUV_levels 
-        Port map( 
-               y_signal1 => y_signal1,
-               y_signal2 => y_signal2,
-               y_alpha   => y_alpha  ,
-               y_result  => y_result ,
-               u_signal1 => u_signal1,
-               u_signal2 => u_signal2,
-               u_alpha   => u_alpha  ,
-               u_result  => u_result ,
-               v_signal1 => v_signal1,
-               v_signal2 => v_signal2,
-               v_alpha   => v_alpha  ,
-               v_result  => v_result 
-               );
+  YUV_out_levels : entity work.YUV_levels
+    port
+    map(
+    y_signal1 => y_signal1,
+    y_signal2 => y_signal2,
+    y_alpha   => y_alpha,
+    y_result  => y_result,
+    u_signal1 => u_signal1,
+    u_signal2 => u_signal2,
+    u_alpha   => u_alpha,
+    u_result  => u_result,
+    v_signal1 => v_signal1,
+    v_signal2 => v_signal2,
+    v_alpha   => v_alpha,
+    v_result  => v_result
+    );
 
 end Behavioral;
